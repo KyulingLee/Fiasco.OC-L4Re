ead91f9f9cd37ef8b90c190e64bf15263073abe4
BogDan Vatra <bogdan@kde.org>
2013-06-08 13:25:24 +0300

    Sync kernel&re

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ mkdir build
 
 cd Fiasco.OC
 
-if [ ! -d ../build/Fiasco.OC]
+if [ ! -d ../build/Fiasco.OC ]
 then
     make BUILDDIR=../build/Fiasco.OC || exit 1
 fi
@@ -28,5 +28,5 @@ then
     make config || exit 1
     touch .l4_configured
 fi
-make -j8
+make -j8 || exit 1
 make qemu MODULE_SEARCH_PATH=$PWD/../Fiasco.OC
